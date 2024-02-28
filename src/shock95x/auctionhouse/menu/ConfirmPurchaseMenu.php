fbf737783e490279842d65cc94c8d8f25d835da3
XackiGiFF <m.girsanoff@gmail.com>
2024-02-29 02:22:11 +0300

    Fixed an error when trying to confirm a purchase without money. Previously, an item was removed from storage even if there were insufficient funds

--- src/shock95x/auctionhouse/menu/ConfirmPurchaseMenu.php
+++ src/shock95x/auctionhouse/menu/ConfirmPurchaseMenu.php
@@ -94,8 +94,6 @@ class ConfirmPurchaseMenu extends AHMenu{
 			$event->call();
 			if($event->isCancelled()) return;
 
-			$storage->removeListing($listing);
-
 			$res = yield from Await::promise(fn($resolve) => $economy->subtractMoney($player, $listing->getPrice(), $resolve));
 			if(!$res){
 				Locale::sendMessage($player, "cannot-afford");
@@ -107,6 +105,7 @@ class ConfirmPurchaseMenu extends AHMenu{
 				Locale::sendMessage($player, "purchase-economy-error");
 				return;
 			}
+            $storage->removeListing($listing);
 			$player->removeCurrentWindow();
 			$player->getInventory()->addItem($item);
 			$player->sendMessage(str_ireplace(["{PLAYER}", "{ITEM}", "{PRICE}", "{AMOUNT}"], [$player->getName(), $item->getName(), $listing->getPrice(true, Settings::formatPrice()), $item->getCount()], Locale::get($player, "purchased-item", true)));
